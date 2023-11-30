09827ad94cfd833042c92f4fcc9bd88ab247bd7e
wntjd612 <wntjdwntjd612@gmail.com>
2023-11-30 15:42:23 +0900

    백엔드

---
+++ app.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+var app = express();
+
+// body-parser 설정
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+// index.html 파일을 루트 경로로 응답
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+// index.css 파일을 스타일 시트로 응답
+app.get('/styles/index.css', function (req, res) {
+    res.sendFile(__dirname + '/styles/index.css');
+});
+
+
+app.post('/api/register', (req, res) => {
+    const address = req.body.address;
+    const place_name = req.body.place_name;
+    const place_kind = req.body.place_kind;
+
+    const newUser = {
+        address: address,
+        place_name: place_name,
+        place_kind: place_kind
+    };
+
+    connection.query('INSERT INTO Users SET ?', newUser, (error, results, fields) => {
+        if (error) {
+            console.log(error);
+            res.status(500).send('Database error');
+        } else {
+            console.log('New user added with ID:', results.insertId);
+            res.status(200).send('User registered successfully');
+        }
+    });
+});
+
+
+app.listen(3000,function (){
+    console.log("open sever");
+});
+
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: '3306',
+    user: 'wntjd',
+    password: '0612',
+    database: 'find_foodtruck'
+});
+
+connection.connect();
+
+const newUser = {
+
+};
+
+
+connection.query('INSERT INTO Users SET ?', newUser, (error, results, fields) => {
+    if (error) throw error;
+    console.log('New user added with ID:', results.insertId);
+});
+
+connection.end();
